3da412ab4394fc03e3f418a128e5287f3b30e6d8
Ichchha <ichchha.codewing@gmail.com>
2022-12-20 23:12:34 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [title,setTitle]=useState('');
+  const todoList = window.localStorage.getItem('todoList');
+  const handleChange = (e) => {
+    e.preventDefault();
+      if (todoList) {
+        const todoListArr = JSON.parse(todoList);
+        todoListArr.push({
+          title
+        });
+        window.localStorage.setItem('todoList', JSON.stringify(todoListArr));
+      }
+      else{
+    window.localStorage.setItem(
+      'todoList',
+      JSON.stringify([
+        {
+          title
+        },
+      ])
+    );
+      }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do list</h1>
+      <input type="text" onChange={(e)=> setTitle(e.target.value)} name="text" value={title}/>
+      <button onClick={(e)=>{
+        handleChange(e)
+      }}>Add</button>
+      {todoList}
     </div>
   );
 }
